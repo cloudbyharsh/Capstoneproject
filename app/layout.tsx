abbecc98e0791f7d7f89fb536567f1f8cf7bf867
Harsh Shah <haarsh.shahh@gmail.com>
2026-06-11 21:35:25 -0400

    fix: resolve Next.js prerender event handler errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,14 +64,22 @@ export default function RootLayout({
                         <Footer />
                   {/* Sender.net Email Capture */}
                         <Script
-                                    id="sender-universal"
-                                    strategy="afterInteractive"
-                                    src="https://cdn.sender.net/accounts_resources/universal.js"
-                                    onLoad={() => {
-                                                  // @ts-expect-error sender is injected by the script
-                                                  if (typeof window.sender === "function") window.sender("1a295a76183580");
-                                    }}
-                                  />
+  id="sender-universal"
+  strategy="afterInteractive"
+  src="https://cdn.sender.net/accounts_resources/universal.js"
+/>
+
+<Script id="sender-init" strategy="afterInteractive">
+  {`
+    window.addEventListener("load", function () {
+      if (typeof window.sender === "function") {
+        window.sender("1a295a76183580");
+      }
+    });
+  `}
+</Script>
+
+
                   {/* Heap Analytics */}
                         <Script id="heap-analytics" strategy="afterInteractive">
                           {`window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};heap.load("2941083674");`}
